0985bc6b5bbc99c4631e7cec677982b9e0cc9312
grzesiek <grzesiekodkrywaja@gmail.com>
2020-08-09 17:38:03 +0200

    post detail

--- motosellapp/views.py
+++ motosellapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Oferta
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Oferta.objects.filter(data_dodania__lte=timezone.now()).order_by('data_dodania')
     return render(request, 'motosellapp/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Oferta, pk=pk)
+    return render(request, 'motosellapp/post_detail.html', {'post': post})
